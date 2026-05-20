e74d278930d4242e8ce8cc7614d42cdbda225591
MAX DIEBANO SILVA ELEUTERIO <00001094711986sp@aluno.educacao.sp.gov.br>
2026-05-20 12:05:26 -0300

    Adicionado Flask e integração com Supabase

--- app.py
+++ app.py
@@ -1,23 +1,6 @@
-import os
-from flask import Flask
-from supabase import create_client
-from dotenv import load_dotenv
+from database.conexao import cadastrar_usuario
 
-load_dotenv()
+print("--- Testando Conexão com o Supabase ---")
 
-app = Flask(__name__)
-
-url = os.getenv("https://jxsgpcfghlodzqebfgoy.supabase.co")
-key = os.getenv("sb_publishable_XVbE5m_vWNtrFTV_jLyfWg_MpWHFa7o")
-
-supabase = create_client(url, key)
-
-@app.route("/")
-def home():
-
-    response = supabase.table("usuarios").select("*").execute()
-
-    return str(response.data)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# Teste de inserção (mude os dados para testar)
+cadastrar_usuario(nome="Seu Amigo", email="amigo@email.com")
--- database/conexao.py
+++ database/conexao.py
@@ -1,9 +1,29 @@
-from supabase import create_client
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url = "https://jxsgpcfghlodzqebfgoy.supabase.co"
+# Carrega o arquivo procurando na raiz do projeto
+load_dotenv(dotenv_path="database.env")
 
-key = "sb_publishable_XVbE5m_vWNtrFTV_jLyfWg_MpWHFa7o"
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
-supabase = create_client(url, key)
+# Esse IF serve para te avisar no terminal se ele falhar em ler as variáveis de novo
+if not url or not key:
+    raise ValueError(
+        "❌ ERRO: Não foi possível ler as credenciais do arquivo database.env. "
+        "Verifique se o arquivo está na raiz do projeto e se os nomes estão corretos!"
+    )
 
-print("Conectado")
+# Inicializa o cliente se estiver tudo OK
+supabase: Client = create_client(url, key)
+
+def cadastrar_usuario(nome: str, email: str):
+    dados_usuario = {"nome": nome, "email": email}
+    try:
+        resposta = supabase.table("usuarios").insert(dados_usuario).execute()
+        print(f"🎉 Usuário {nome} cadastrado com sucesso!")
+        return resposta.data
+    except Exception as e:
+        print(f"❌ Erro ao cadastrar usuário: {e}")
+        return None
