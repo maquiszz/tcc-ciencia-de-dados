bcaeead21d220088db3c5705e0ab0b2835447869
LEANDRO ALVES DE ALMEIDA <00001118513319sp@aluno.educacao.sp.gov.br>
2026-05-28 10:40:47 -0300

    Limpando arquivos ignorados

--- app.py
+++
@@ -1,6 +0,0 @@
-from database.conexao import cadastrar_usuario
-
-print("--- Testando Conexão com o Supabase ---")
-
-# Teste de inserção (mude os dados para testar)
-cadastrar_usuario(nome="Seu Amigo", email="amigo@email.com")
